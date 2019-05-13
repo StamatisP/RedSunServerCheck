6ecf6cd8b80c7c3c565896126b2698df8724b638
Stamos Prekas <prekasstamos@gmail.com>
2019-05-12 20:28:55 -0400

    Work on timer

--- RedSunServerCheck/Form1.Designer.cs
+++ RedSunServerCheck/Form1.Designer.cs
@@ -28,13 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(33, 385);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(184, 53);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Send Request";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("TF2 Build", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(775, 182);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- RedSunServerCheck/Form1.cs
+++ RedSunServerCheck/Form1.cs
@@ -7,14 +7,93 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Timers;
 
 namespace RedSunServerCheck
 {
     public partial class Form1 : Form
     {
+
+        public class ServerInfo
+        {
+            public string name { get; set; }
+            public string gamemode { get; set; }
+            public string map { get; set; }
+            public int players { get; set; }
+            public int maxplayers { get; set; }
+
+            public static implicit operator ServerInfo(A2S_INFO server)
+            {
+                return new ServerInfo { name = server.Name, gamemode = server.Game, map = server.Map, maxplayers = server.MaxPlayers, players = server.Players };
+            }
+        }
+
+        private System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
+
+        IPAddress raidenAddr = IPAddress.Parse("95.172.92.5");
+        IPEndPoint raidenEndpoint;
+
+        IPAddress armstrongAddr = IPAddress.Parse("192.223.24.11");
+        IPEndPoint armstrongEndpoint;
+
         public Form1()
         {
             InitializeComponent();
+            raidenEndpoint = new IPEndPoint(raidenAddr, 27015);
+            armstrongEndpoint = new IPEndPoint(armstrongAddr, 27015);
+
+            try
+            {
+                timer.Tick += new EventHandler(RequestInfoTimer);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            timer.Interval = 5000;
+            timer.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RequestInfo();
+        }
+
+        public void RequestInfo()
+        {
+            A2S_INFO raidenInfo = new A2S_INFO(raidenEndpoint);
+            A2S_INFO armstrongInfo = new A2S_INFO(armstrongEndpoint);
+
+            textBox1.Text = "";
+            FormatTextbox((ServerInfo)raidenInfo);
+            FormatTextbox((ServerInfo)armstrongInfo);
+        }
+
+        void RequestInfoTimer(Object source, EventArgs e)
+        {
+            RequestInfo();
+        }
+            
+        void FormatTextbox(ServerInfo info)
+        {
+            NewlineAppend(info.name + " - " + info.gamemode);
+            NewlineAppend("    " + info.players.ToString() + "/" + info.maxplayers.ToString());
+            NewlineAppend("    " + info.map);
+            NewlineAppend("");
+            NewlineAppend("");
+        }
+
+        void NewlineAppend(string line)
+        {
+            textBox1.AppendText(line);
+            textBox1.AppendText(Environment.NewLine);
         }
     }
 }
